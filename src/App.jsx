48852466e2bd055a1b3521b09a01414b569b98cd
Lazizbek <oddiybolakay05@gmail.com>
2024-10-10 16:02:56 +0500

    my-cv

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PDFViewer from "./pages/home/home";
+
+function App() {
+  return (
+    <>
+      <PDFViewer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const PDFViewer = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/src/assets/files/laziz-khamidov-cv.pdf";
+    link.download = "laziz-khamidov-cv.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+      <div className=" py-10 max-w-screen-lg w-[90%] min-h-screen text-center">
+        <iframe
+          src="/src/assets/files/laziz-khamidov-cv.pdf"
+          title="PDF Viewer"
+          className="w-full max-h-[600px] h-screen min-h-[500px] rounded-lg"
+        ></iframe>
+
+        <button
+          onClick={handleDownload}
+          className="mt-5 align-middle px-6 py-3 text-white bg-blue-600 rounded-lg shadow-md hover:bg-green-500 transition-all"
+        >
+          Download PDF
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PDFViewer;
